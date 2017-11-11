e938a626451719a894028bb66ec2da14a58f90a2
Ashish Malav <aashmalav@gmail.com>
2017-11-10 20:42:21 -0600

    added channels and messages aaray to seed file

--- seed.js
+++ seed.js
@@ -2,7 +2,7 @@ const {Author, Message, Channel} = require('./server/db/models');
 const db = require('./server/db');
 
 
-const Authors = [
+const authors = [
   {
     name: 'Anna',
     image: 'images/girl.jpeg',
@@ -38,5 +38,101 @@ const Authors = [
   {
     name: 'John',
     image: 'images/clown.jpeg',
+  },
+];
+
+
+const channels = [
+  {
+    name: 'random',
+  },
+  {
+    name: 'tigers',
+  },
+  {
+    name: 'cavaliers',
+  },
+  {
+    name: 'vikings',
+  },
+  {
+    name: 'indians',
+  },
+];
+
+const getAuthorId = () => Math.floor(Math.random() * authors.length - 1) + 1;
+
+const getChannelId = () => Math.floor(Math.random() * channels.length - 1) + 1;
+
+const messages = [
+  {
+    authorId: getAuthorId(),
+    content: 'I like React!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like Redux!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like Express!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like Node.js!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like Semantic UI!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like Bulma!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like Victory.js!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like D3.js!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like PostgreSQL!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I like Sequelize.js!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I love tacos!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I love enchilladas!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I love refried beans!',
+    channelId: getChannelId(),
+  },
+   {
+    authorId: getAuthorId(),
+    content: 'I love burritos!',
+    channelId: getChannelId(),
   },
 ];
