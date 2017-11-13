13198025b1fabcafdf129b5c1ae6b58d3851b33c
Ashish Malav <aashmalav@gmail.com>
2017-11-13 15:46:23 -0600

    written test case for author model and passed

---
+++ tests/models/author.spec.js
@@ -0,0 +1,31 @@
+/* describe(), beforEach() and it() are available globally  */
+
+const {expect} = require('chai');
+const db = require('../../server/db');
+const {Author} = require('../../server/db/models');
+
+describe('Author Model', () => {
+  beforeEach(() => {
+    db.sync({force: true})
+      .then(() => {
+        console.log('Test DB synced');
+      });
+  });
+
+  describe('create author', () => {
+    let michael;
+    beforeEach(() => {
+      return Author.create({
+        name: 'Michael',
+        image: 'michaelimage',
+      })
+      .then(author => {
+        michael = author;
+      });
+    });
+
+    it('should create author with name Michael', () => {
+      expect(michael.name).to.be.equal('Michael');
+    })
+  });
+});
