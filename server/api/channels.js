6868c54ded0da648f16f0219d84abe21ebf71761
Ashish Malav <aashmalav@gmail.com>
2017-11-14 19:56:24 -0600

    GET api/channels request is responding with all channels

--- server/api/channels.js
+++ server/api/channels.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
+const {Channel} = require('../db/models');
 module.exports = router;
 
+//GET api/channels
+router.get('/', (req, res, next) => {
+  Channel.findAll()
+    .then(channels => res.json(channels))
+    .catch(next);
+});
--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/messages', require('./messages'));
-router.use('./channels', require('./channels'));
+router.use('/channels', require('./channels'));
 
 //if route doesn't macth
 router.use((req, res, next) => {
