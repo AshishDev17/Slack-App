36a7a7f93546296f861a54dc809f832f3b5cfca2
Ashish Malav <aashmalav@gmail.com>
2017-11-16 15:05:36 -0600

    GET api/messagess request is responding with all authors

--- server/api/messages.js
+++ server/api/messages.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
+const {Message} = require('../db/models');
 module.exports = router;
 
-
+//GET api/messages
+router.get('/', (req, res, next) => {
+  Message.findAll()
+    .then(messages => res.json(messages))
+    .catch(next);
+});
 
